dd0bca6a851a43744b1ac85f77057dc6460c6e19
tomszociepa <tom.ociepa@gmail.com>
2023-12-29 12:34:23 +0100

    L145 AuthModule(FeatureModule)+ komponenty do logowania i rejestracji

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './modules/core/core.module';
 import { HomeModule } from './modules/home/home.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, CoreModule, HomeModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    CoreModule,
+    HomeModule,
+    AuthModule,
+    AppRoutingModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
