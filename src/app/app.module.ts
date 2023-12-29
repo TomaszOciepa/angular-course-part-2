d669854bdc43ddfaae8ae4f11b33f45ef7566bac
tomszociepa <tom.ociepa@gmail.com>
2023-12-29 12:43:46 +0100

    L146 ClientsModule (FeatureModule)+ komponent z lista klientow

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { CoreModule } from './modules/core/core.module';
 import { HomeModule } from './modules/home/home.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { ClientsModule } from './modules/clients/clients.module';
 
 @NgModule({
   declarations: [AppComponent],
@@ -14,6 +15,7 @@ import { AuthModule } from './modules/auth/auth.module';
     CoreModule,
     HomeModule,
     AuthModule,
+    ClientsModule,
     AppRoutingModule,
   ],
   providers: [],
