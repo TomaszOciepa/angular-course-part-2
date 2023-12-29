b7801366dc1a87508598d01b72e08a9e93a6a62d
tomszociepa <tom.ociepa@gmail.com>
2023-12-29 14:13:08 +0100

    L149 Utworzenie formularza oraz walidacja (LoginComponent)

--- src/app/modules/auth/components/login/login.component.ts
+++ src/app/modules/auth/components/login/login.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { UserLoginData } from 'src/app/modules/core/models/user.model';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  hide = true;
+  userData: UserLoginData = {
+    username: '',
+    password: '',
+  };
 
+  onLogin() {
+    console.log(this.userData);
+  }
 }
