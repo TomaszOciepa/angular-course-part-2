dec36652e5f716c66c123b5d6ee2642243ddef18
tomszociepa <tom.ociepa@gmail.com>
2023-12-31 12:49:34 +0100

    L156 śledzenie zmian w kontrolce valueChanges

--- src/app/modules/auth/components/register/register.component.ts
+++ src/app/modules/auth/components/register/register.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { NotExpr } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
@@ -6,13 +7,19 @@ import { FormControl, FormGroup } from '@angular/forms';
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss'],
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
   hide = true;
   registerForm = new FormGroup({
     email: new FormControl(''),
     username: new FormControl(''),
     password: new FormControl(''),
   });
+
+  ngOnInit(): void {
+    this.registerForm.controls.email.valueChanges.subscribe((text) => {
+      console.log(text);
+    });
+  }
 
   onRegister() {
     console.log(this.registerForm.value);
