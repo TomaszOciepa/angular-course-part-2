7101b2d048379ca4b49365ab671820bee9b2e837
tomszociepa <tom.ociepa@gmail.com>
2023-12-31 13:23:06 +0100

    L159 Walidacja w Reactive Form (z Angular Material)

--- src/app/modules/auth/components/register/register.component.ts
+++ src/app/modules/auth/components/register/register.component.ts
@@ -1,6 +1,5 @@
-import { NotExpr } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -10,21 +9,45 @@ import { FormControl, FormGroup } from '@angular/forms';
 export class RegisterComponent implements OnInit {
   hide = true;
   registerForm = new FormGroup({
-    email: new FormControl(''),
-    username: new FormControl(''),
-    password: new FormControl(''),
+    email: new FormControl('', [
+      Validators.email,
+      Validators.minLength(5),
+      Validators.maxLength(50),
+    ]),
+    username: new FormControl('', [Validators.required]),
+    password: new FormControl('', { validators: [Validators.required] }),
   });
 
+  get controls() {
+    return this.registerForm.controls;
+  }
   ngOnInit(): void {
     // this.registerForm.controls.email.valueChanges.subscribe((text) => {
     //   console.log(text);
     // });
     console.log('');
-    this.registerForm.controls.email.disable();
+    this.registerForm.controls.email.hasError('email');
+    // this.registerForm.controls.email.disable();
+  }
+
+  getErrorMessage(control: FormControl) {
+    if (control.hasError('required')) {
+      return 'Musisz wpisać jakąś wartość!';
+    }
+
+    if (control.hasError('minlength')) {
+      return 'Przekazałeś za mało znaków w kontrolce';
+    }
+
+    if (control.hasError('maxlength')) {
+      return 'Przekazałeś za duzo znaków w kontrolce';
+    }
+
+    return control.hasError('email') ? 'Nieprwidłowy adres email' : '';
   }
 
   onRegister() {
     console.log(this.registerForm.value);
-    console.log(this.registerForm.getRawValue());
+    // console.log(this.registerForm.getRawValue());
   }
 }
