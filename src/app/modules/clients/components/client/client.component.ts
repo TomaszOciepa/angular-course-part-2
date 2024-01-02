d4e757203280cb2800bd6099b3025606824deb98
tomszociepa <tom.ociepa@gmail.com>
2024-01-02 17:41:33 +0100

    L174 ClientComponent - pobieranie klienta na podstawie id z url

--- src/app/modules/clients/components/client/client.component.ts
+++ src/app/modules/clients/components/client/client.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs';
+import { ClientsService } from 'src/app/modules/core/services/clients.service';
 
 @Component({
   selector: 'app-client',
   templateUrl: './client.component.html',
-  styleUrls: ['./client.component.scss']
+  styleUrls: ['./client.component.scss'],
 })
-export class ClientComponent {
+export class ClientComponent implements OnInit {
+  constructor(
+    private clientsService: ClientsService,
+    private route: ActivatedRoute,
+  ) {}
 
+  ngOnInit(): void {
+    this.route.params
+      .pipe(switchMap((params) => this.clientsService.getClient(+params['id'])))
+      .subscribe({
+        next: (client) => {
+          console.log(client);
+        },
+      });
+  }
 }
