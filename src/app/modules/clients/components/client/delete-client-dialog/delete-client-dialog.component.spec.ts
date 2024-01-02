9df3d80e05372c01f2bb8574f021a778bb51bad5
tomszociepa <tom.ociepa@gmail.com>
2024-01-02 19:25:53 +0100

    L178 Utworzenie metody do usuwania klienta

---
+++ src/app/modules/clients/components/client/delete-client-dialog/delete-client-dialog.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DeleteClientDialogComponent } from './delete-client-dialog.component';
+
+describe('DeleteClientDialogComponent', () => {
+  let component: DeleteClientDialogComponent;
+  let fixture: ComponentFixture<DeleteClientDialogComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [DeleteClientDialogComponent]
+    });
+    fixture = TestBed.createComponent(DeleteClientDialogComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/modules/clients/components/client/delete-client-dialog/delete-client-dialog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-client-dialog',
+  templateUrl: './delete-client-dialog.component.html',
+  styleUrls: ['./delete-client-dialog.component.scss']
+})
+export class DeleteClientDialogComponent {
+
+}
