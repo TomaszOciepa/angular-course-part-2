ff5cdb599725712014ca527d14d7ea08f842da81
tomszociepa <tom.ociepa@gmail.com>
2024-01-04 15:58:32 +0100

    L194 Guard canDeactivate (w postaci funkcji)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { authGuardLoad } from './modules/core/guards/auth-load.guard';
 
 const routes: Routes = [
   {
@@ -12,6 +13,7 @@ const routes: Routes = [
     path: 'klienci',
     loadChildren: () =>
       import('./modules/clients/clients.module').then((m) => m.ClientsModule),
+    canMatch: [authGuardLoad],
   },
 ];
 
--- src/app/modules/clients/clients-routing.module.ts
+++ src/app/modules/clients/clients-routing.module.ts
@@ -3,14 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { ClientsComponent } from './clients.component';
 import { ClientComponent } from './components/client/client.component';
 import { ClientFormComponent } from './components/client-form/client-form.component';
-import { authActivateGuard } from '../core/guards/auth-activate.guard';
+import { clientFormDeactivateGuard } from '../core/guards/client-form-deactivate.guard';
 
 const routes: Routes = [
-  { path: '', component: ClientsComponent, canActivate: [authActivateGuard] },
+  {
+    path: '',
+    component: ClientsComponent,
+  },
   {
     path: 'dodaj',
     component: ClientFormComponent,
-    canActivate: [authActivateGuard],
+    canDeactivate: [clientFormDeactivateGuard],
   },
   { path: ':id', component: ClientComponent },
 ];
